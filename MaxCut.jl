f1716f203d39f830dc8631769d906bde3608de21
Eric Proffitt <esproff@gmail.com>
2016-08-19 19:06:01 -0700

    update MaxCut

--- MaxCut.jl
+++ MaxCut.jl
@@ -1,32 +1,33 @@
-using Convex
-using SCS
+using Convex # Pkg.add("Convex")
+using SCS # Pkg.add("SCS")
 
-"MAXCUT";
+"MAXCUT"
 
 "Partition a graph into two disjoint sets such that the sum of the edge weights
-from all edges which cross the partition is as large as possible (known to be NP-hard).";
+from all edges which cross the partition is as large as possible (known to be NP-hard)."
 
-function goemansWilliamson{T <: Real}(W::Array{T, 2}; tol::Real=1e-1, iter::Int=100)
+function goemansWilliamson{T<:Real}(W::Matrix{T}; tol::Real=1e-1, iter::Int=100)
 	"A cut of a graph can be produced by assigning either 1 or -1 to each vertex.  The Goemans-Williamson 
 	algorithm relaxes this binary condition to allow for vector assignments drawn from the (n-1)-sphere
 	(choosing an n-1 dimensional space will insure seperability).  This relaxation can then be written as 
 	an SDP.  Once the optimal vector assignments are found, origin centered hyperplanes are generated and 
 	their corresponding cuts evaluated.  After 'iter' trials, or when the desired tolerance is reached,
 	which ever comes first, the hyperplane with the highest corresponding binary cut is used to partition 
-	the vertices.";
-	"W:		Adjacency matrix.";
-	"tol:	Maximum acceptable distance between a cut and the MAXCUT upper bound.";
-	"iter:	Maximum number of hyperplane iterations before a cut is chosen.";
+	the vertices."
+	"W:		Adjacency matrix."
+	"tol:	Maximum acceptable distance between a cut and the MAXCUT upper bound."
+	"iter:	Maximum number of hyperplane iterations before a cut is chosen."
+
 	LinAlg.chksquare(W)
-	LinAlg.issym(W)		|| error("Adjacency matrix must be symmetric.")
-	all(W .>= 0)		|| error("Entries of the adjacency matrix must be nonnegative.")
-	all(diag(W) .== 0)	|| error("Diagonal entries of adjacency matrix must be zero.")
-	tol > 0				|| error("The tolerance 'tol' must be positive.")
-	iter > 0			|| error("The number of iterations 'iter' must be at least one.")
+	@assert LinAlg.issym(W)		"Adjacency matrix must be symmetric."
+	@assert all(W .>= 0)		"Entries of the adjacency matrix must be nonnegative."
+	@assert all(diag(W) .== 0)	"Diagonal entries of adjacency matrix must be zero."
+	@assert tol > 0				"The tolerance 'tol' must be positive."
+	@assert iter > 0			"The number of iterations 'iter' must be a positive integer."
 
 	"This is the standard SDP Relaxation of the MAXCUT problem, a reference can be found at
-	http://www.sfu.ca/~mdevos/notes/semidef/GW.pdf.";
-	const k = size(W, 1)
+	http://www.sfu.ca/~mdevos/notes/semidef/GW.pdf."
+	k = size(W, 1)
 	S = Semidefinite(k)
 	
 	expr = vecdot(W, S)
@@ -35,9 +36,9 @@ function goemansWilliamson{T <: Real}(W::Array{T, 2}; tol::Real=1e-1, iter::Int=
 	solve!(problem, SCSSolver(verbose=0))
 	
 	X = full(cholfact(S.value, :U, Val{true}))
-	upperbound = (sum(W) - vecdot(W, S.value))/4 # A non-trivial upper bound on MAXCUT.
+	upperbound = (sum(W) - vecdot(W, S.value)) / 4 # A non-trivial upper bound on MAXCUT.
 
-	"Random origin-centered hyperplanes, generated to produce partitions of the graph.";
+	"Random origin-centered hyperplanes, generated to produce partitions of the graph."
 	maxcut = 0
 	maxpartition = nothing
 
@@ -50,11 +51,11 @@ function goemansWilliamson{T <: Real}(W::Array{T, 2}; tol::Real=1e-1, iter::Int=
 			maxpartition = partition
 			maxcut = cut
 		end
-		
-		if (upperbound - maxcut) < tol; break; end
-		if i == iter; println("Max iterations reached."); end
+
+		upperbound - maxcut < tol && break
+		i == iter && println("Max iterations reached.")
 	end
-	return round(maxcut,3), maxpartition
+	return round(maxcut, 3), maxpartition
 end
 
 function test()
@@ -63,9 +64,11 @@ function test()
 		 2 3 0 0 0; 
 		 1 2 0 0 4; 
 		 0 0 0 4 0]
+	
 	maxcut, maxpartition = goemansWilliamson(W)
-	println(maxcut)
-	println(maxpartition)
+	@show maxcut
+	@show maxpartition
+	nothing
 end
 
 
