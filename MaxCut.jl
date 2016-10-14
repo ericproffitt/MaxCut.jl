2ffd43b83abbdf3d3ecc02f2082fdb2aa41f6225
esproff <esproff@gmail.com>
2016-10-14 08:41:36 -0700

    Update MaxCut.jl

--- MaxCut.jl
+++ MaxCut.jl
@@ -3,10 +3,10 @@ using SCS # Pkg.add("SCS")
 
 "Max-Cut"
 
-"Partition a graph into two disjoint sets such that the sum of the edge weights
-from all edges which cross the partition is as large as possible (known to be NP-hard)."
-
 function goemansWilliamson{T<:Real}(W::Matrix{T}; tol::Real=1e-1, iter::Int=100)
+	"Partition a graph into two disjoint sets such that the sum of the edge weights
+	from all edges which cross the partition is as large as possible (known to be NP-hard)."
+
 	"A cut of a graph can be produced by assigning either 1 or -1 to each vertex.  The Goemans-Williamson 
 	algorithm relaxes this binary condition to allow for vector assignments drawn from the (n-1)-sphere
 	(choosing an n-1 dimensional space will ensure seperability).  This relaxation can then be written as 
@@ -18,12 +18,12 @@ function goemansWilliamson{T<:Real}(W::Matrix{T}; tol::Real=1e-1, iter::Int=100)
 	"tol:	Maximum acceptable distance between a cut and the Max-Cut upper bound."
 	"iter:	Maximum number of hyperplane iterations before a cut is chosen."
 
-	LinAlg.chksquare(W)
-	@assert LinAlg.issym(W)		"Adjacency matrix must be symmetric."
-	@assert all(W .>= 0)		"Entries of the adjacency matrix must be nonnegative."
-	@assert all(diag(W) .== 0)	"Diagonal entries of adjacency matrix must be zero."
-	@assert tol > 0				"The tolerance 'tol' must be positive."
-	@assert iter > 0			"The number of iterations 'iter' must be a positive integer."
+	LinAlg.checksquare(W)
+	@assert LinAlg.issymmetric(W)	"Adjacency matrix must be symmetric."
+	@assert all(W .>= 0)			"Entries of the adjacency matrix must be nonnegative."
+	@assert all(diag(W) .== 0)		"Diagonal entries of adjacency matrix must be zero."
+	@assert tol > 0					"The tolerance 'tol' must be positive."
+	@assert iter > 0				"The number of iterations 'iter' must be a positive integer."
 
 	"This is the standard SDP Relaxation of the Max-Cut problem, a reference can be found at
 	http://www.sfu.ca/~mdevos/notes/semidef/GW.pdf."
@@ -34,8 +34,11 @@ function goemansWilliamson{T<:Real}(W::Matrix{T}; tol::Real=1e-1, iter::Int=100)
 	constr = [S[i,i] == 1.0 for i in 1:k]
 	problem = minimize(expr, constr...)
 	solve!(problem, SCSSolver(verbose=0))
-	
-	X = full(cholfact(S.value, :U, Val{true}))
+
+	A = 0.5 * (S.value + S.value') # Ensure symmetric.
+	A += max(0, -eigmin(A)) * eye(size(A, 1)) + eps(1.0) # Ensure positive-definite.
+
+	X = full(chol(A))
 	upperbound = (sum(W) - vecdot(W, S.value)) / 4 # A non-trivial upper bound on Max-Cut.
 
 	"Random origin-centered hyperplanes, generated to produce partitions of the graph."
@@ -70,6 +73,4 @@ function test()
 	@show maxpartition
 	nothing
 end
-
-
 
