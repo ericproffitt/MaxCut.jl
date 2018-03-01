ca9801fa39ccda4f84b7c3ca67619280e0fa01a2
Eric Proffitt <ericproff@icloud.com>
2018-03-01 04:49:55 -0500

    Update and rename MaxCut.jl to maxcut.jl

--- maxcut.jl
+++ maxcut.jl
@@ -1,21 +1,21 @@
-using Convex # Pkg.add("Convex")
-using SCS # Pkg.add("SCS")
+using Convex
+using SCS
 
-"Max-Cut"
+"The Goemans-Williamson algorithm for the MAXCUT problem."
 
 function goemansWilliamson{T<:Real}(W::Matrix{T}; tol::Real=1e-1, iter::Int=100)
 	"Partition a graph into two disjoint sets such that the sum of the edge weights
-	from all edges which cross the partition is as large as possible (known to be NP-hard)."
+	which cross the partition is as large as possible (known to be NP-hard)."
 
 	"A cut of a graph can be produced by assigning either 1 or -1 to each vertex.  The Goemans-Williamson 
 	algorithm relaxes this binary condition to allow for vector assignments drawn from the (n-1)-sphere
 	(choosing an n-1 dimensional space will ensure seperability).  This relaxation can then be written as 
 	an SDP.  Once the optimal vector assignments are found, origin centered hyperplanes are generated and 
 	their corresponding cuts evaluated.  After 'iter' trials, or when the desired tolerance is reached,
-	which ever comes first, the hyperplane with the highest corresponding binary cut is used to partition 
-	the vertices."
+	the hyperplane with the highest corresponding binary cut is used to partition the vertices."
+	
 	"W:		Adjacency matrix."
-	"tol:	Maximum acceptable distance between a cut and the Max-Cut upper bound."
+	"tol:	Maximum acceptable distance between a cut and the MAXCUT upper bound."
 	"iter:	Maximum number of hyperplane iterations before a cut is chosen."
 
 	LinAlg.checksquare(W)
@@ -25,7 +25,7 @@ function goemansWilliamson{T<:Real}(W::Matrix{T}; tol::Real=1e-1, iter::Int=100)
 	@assert tol > 0					"The tolerance 'tol' must be positive."
 	@assert iter > 0				"The number of iterations 'iter' must be a positive integer."
 
-	"This is the standard SDP Relaxation of the Max-Cut problem, a reference can be found at
+	"This is the standard SDP Relaxation of the MAXCUT problem, a reference can be found at
 	http://www.sfu.ca/~mdevos/notes/semidef/GW.pdf."
 	k = size(W, 1)
 	S = Semidefinite(k)
@@ -35,19 +35,22 @@ function goemansWilliamson{T<:Real}(W::Matrix{T}; tol::Real=1e-1, iter::Int=100)
 	problem = minimize(expr, constr...)
 	solve!(problem, SCSSolver(verbose=0))
 
-	A = 0.5 * (S.value + S.value') # Ensure symmetric.
-	A += max(0, -eigmin(A)) * eye(size(A, 1)) + eps(1.0) # Ensure positive-definite.
+	### Ensure symmetric positive-definite.
+	A = 0.5 * (S.value + S.value')
+	A += max(0, -eigmin(A)) * eye(size(A, 1)) + eps(1e3)
 
 	X = full(chol(A))
-	upperbound = (sum(W) - vecdot(W, S.value)) / 4 # A non-trivial upper bound on Max-Cut.
+
+	### A non-trivial upper bound on MAXCUT.
+	upperbound = (sum(W) - vecdot(W, S.value)) / 4 
 
 	"Random origin-centered hyperplanes, generated to produce partitions of the graph."
 	maxcut = 0
 	maxpartition = nothing
 
 	for i in 1:iter
-		eval = X' * randn(k)
-		partition = (find(eval .>= 0), find(eval .< 0))
+		gweval = X' * randn(k)
+		partition = (find(gweval .>= 0), find(gweval .< 0))
 		cut = sum(W[partition...])
 
 		if cut > maxcut
@@ -73,4 +76,3 @@ function test()
 	@show maxpartition
 	nothing
 end
-
